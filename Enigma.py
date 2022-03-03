2f0616e8284772bb08a1c06686665d99eda9105f
Vistu55 <100876931+Vistu55@users.noreply.github.com>
2022-03-03 17:52:22 -0300

    Create Enigma.py

---
+++ Enigma.py
@@ -0,0 +1,211 @@
+import os
+from discord_easy_commands import EasyBot
+bot = EasyBot()
+bot.setCommand("Hola","""
+ Hola, soy Enigma, el bot de The cave of Vistu
+ ¿Sos nuevo?
+
+  Responde si o no             
+               """)
+bot.setCommand("Holis","""
+ Hola, soy Enigma, el bot de The cave of Vistu
+ ¿Sos nuevo?
+
+  Responde si o no             
+               """)
+bot.setCommand("Q onda","""
+ Hola, soy Enigma, el bot de The cave of Vistu
+ ¿Sos nuevo?
+
+  Responde si o no             
+               """)
+bot.setCommand("Hola Enigma","""
+ Hola, soy Enigma, el bot de The cave of Vistu
+ ¿Sos nuevo?
+
+  Responde si o no             
+               """)
+bot.setCommand("Enigma","""
+ Hola, soy Enigma, el bot de The cave of Vistu
+ ¿Sos nuevo?
+
+  Responde si o no             
+               """)
+bot.setCommand("Q onda Enigma","""
+ Hola, soy Enigma, el bot de The cave of Vistu
+ ¿Sos nuevo?
+
+  Responde si o no             
+               """)
+bot.setCommand(".","""
+ Hola, soy Enigma, el bot de The cave of Vistu
+ ¿Sos nuevo?
+
+  Responde si o no             
+               """)
+bot.setCommand("Hey Enigma","""
+ Hola, soy Enigma, el bot de The cave of Vistu
+ ¿Sos nuevo?
+
+  Responde si o no             
+               """)
+bot.setCommand("si","""
+Bienvenido/a al servidor!! Espero que la pases re bien
+Acá te dejo un menú para que puedas interactuar.
+
+ Pulsa 1 para ver las reglas basicas del servidor
+               
+ Pulsa 2 para ver todos los roles del servidor
+               
+ Pulsa 3 para generar una invitacion al servidor
+               
+ Pulsa 4 para ver todos los canales de voz y de texto del servidor
+               
+ Pulsa 5 para mas datos sobre mi!!  
+
+ Pulsa 6 para ver la coleccion de NFTs de Vistu55#3038!!              
+ Si querés podemos volver al menú
+ Escribí menu si queres volver
+  Si queres abandonar, escribí adios             
+               
+               """)
+
+bot.setCommand("1","""
+Las normativas basicas del servidor son
+No pelear con nadie
+No burlarse de los demás
+No faltar el respeto a NADIE
+No discriminar a nadie por ninguna condicion
+No joder con ningun tema serio
+Se admitirán todo tipo de chistes de humor negro         
+
+ Para mas reglas ir al canal de #𝑅𝑢𝑙𝑒𝑠   
+               
+               """)
+bot.setCommand("2","""
+Todos los roles del servidor son
+Vistu: solo Vistu55#3038 tiene este rol
+               
+The admin role: solo 3 personas tienen este rol 
+               
+Boy: este rol lo tienen todos los chicos del servidor, 20 personas tienen este rol
+               
+Girl: este rol lo tienen todas las chicas del servidor, 10 personas tienen este rol
+               
+J: solo Josefinamartinezz#7851 tiene este rol  
+               
+BOT: solo los bots del servidor tienen este rol, 9 usuarios tienen este rol
+               
+ Invitado temporal: los invitados temporales tienen este rol, solo 2 personas tienen este rol
+               
+ Permanente: las personas que se quedaran permanentemente en el servidor tienen este rol. 13 personas tienen este rol
+               
+ Activo: las personas mas activas tienen este rol. 13 personas tienen este rol
+               
+ Fortnite player: Las personas que juegan Fortnite tienen este rol. 11 personas tienen este rol                                                     
+               """)
+bot.setCommand("3","https://discord.gg/e3PeQhmb")
+bot.setCommand("4","""
+Canales de texto: 17
+Canales de voz: 11              
+               """)
+bot.setCommand("5","""
+Me llamo Enigma
+               
+Fui programado el 1 de Marzo de 2022 por Vistu55#3038
+               
+Mi color favorito es el celeste
+               
+Amo hacer nuevos amigos
+               
+Me gusta mucho ir a pasear por los servidores!!
+               
+Amo compartir servidores con mas bots
+               
+Amo que la gente me invite a sus servidores :grin: :heart:
+
+ Queres tener un BOT como yo en tu servidor? Comunicate con el creador:
+               
+ Discord: Vistu55#3038
+               
+ Instagram: https://www.instagram.com/vistuchi07/?hl=es-la
+               
+ Twitter: https://twitter.com/IamVistu55              
+               """)
+bot.setCommand("6","""
+ Acá está la lista de NFTs de Vistu55#3038 para que mires... y para que compres... digo... nada, nada  :flushed: :smirk: 
+
+ Link: https://mintable.app/u/vistuchi55               
+               """)
+
+
+bot.setCommand("no","""
+ Quen te gustaría saber?
+
+ Pulsa A para ver como me crearon
+               
+ Pulsa B para conocer a mi creador
+               
+ Pulsa C para hablar en el canal principal
+               
+ Pulsa D para proponer nuevas ideas en el servidor!!
+               
+ Pulsa E para aprender a programar tu propio BOT de comandos
+               
+ Pulsa F para aprender a programar tu propio BOT de música
+
+ Pulsa G para conocer la historia del servidor 
+            
+ 
+Si querés podemos volver al menú!!
+               
+ Escribí menu si queres volver
+               
+  Si queres abandonar, escribí adios               
+               """)
+bot.setCommand("A","""
+ Fui programado con el programa Python, al principio iba a ser un BOT para reproducir música en el servidor, pero no fue asi :joy:. Poco a poco ire teniendo mas funciones para que puedas interactuar conmigo!! 
+               
+               """)
+bot.setCommand("B","""
+Mi creador se llama Fran, mas conocido como Vistu o Vista. Le encanta programar y le interesa mucho ese tema!! Poco a poco iremos avanzando e iremos haciendo mas cosas...proximamente!!
+               
+               
+               """)
+bot.setCommand("C","""
+Hace click acá #🇱🇴🇧🇧🇾 para hablar en el chat!!               
+                """)
+bot.setCommand("D","""
+ Queres proponer nuevas ideas para el servidor? Contanos tus sugerencias acá #sugerencias              
+               """)
+bot.setCommand("E","""
+ Te dejo el link de un tutorial básico de como programar tu propio BOT de comandos para tu servidor!!                                                                       Link: https://youtu.be/PCrYCj0h3SI
+               
+               
+               """)
+bot.setCommand("F","""
+Queres escuchar música con tus amigos en un canal de voz? Entonces esta opcion es para vos!! Te dejo el link de un tutorial básico de como programar tu propio BOT de música para tu servidor!! 
+
+Link: https://www.youtube.com/watch?v=jb8lnBQgTzA               
+               """)
+bot.setCommand("G","""
+ Nos remontamos a septiembre de 2021 cuando a Vistu55#3038 se le ocurrió la idea de hacer un nuevo servidor con el fin de tener un servidor mas variado y con mas personas. Poco a poco fuimos creciendo hasta hoy!! Ojala esto dure para siempre y muchas gracias a todos :blue_heart:
+               
+               """)
+bot.setCommand("menu","""
+Ok, volvamos!!
+
+Hola, soy Enigma, el bot de The cave of Vistu
+ ¿Sos nuevo?
+
+  Responde si o no                 
+               """)
+
+bot.setCommand("adios","""
+Adiós, gracias por interactuar conmigo, te mando un cyber abrazo y ojalá que nos volvamos a encontrar en otra ocasión.
+
+ Gracias por usar Enigma
+
+               Vistu
+               """)
+bot.run(os.environ['TOKEN'])
